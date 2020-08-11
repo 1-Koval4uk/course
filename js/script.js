daea6c881030770f9628a2050c9d38fbf362f895
1-Koval4uk <46053952+1-Koval4uk@users.noreply.github.com>
2020-08-11 15:13:11 +0300

    third commit

--- js/script.js
+++ js/script.js
@@ -1,42 +1,78 @@
-'use strict'
-// 1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+// 1) Первую часть задания повторить по уроку
 
-// 2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-// отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-// возвращаем пользователя к вопросам опять
+// 2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+// false - выводит в консоль главный объект программы
 
-// 3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-// "Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-// "Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+// 3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+// "Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+// genres
 
-// 4) Потренироваться и переписать цикл еще двумя способами*/
+// P.S. Функции вызывать не обязательно*/
+'use strict';
+let numberOfFilms;
+
+function start(){
+    numberOfFilms = +prompt('How many films u watched?', '0');
+    while(numberOfFilms == '' || isNaN(numberOfFilms) || numberOfFilms == null){
+        numberOfFilms = +prompt('How many films u watched?', '0');
+    }
+}
+start();
 
 
 
-let numberOfFilms = prompt('How many films u watched?', '0')
 let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
-}
+};
 
-for(let i = 0; i < 2; i++){
-    let a = prompt('what is the name of film?', '');
-    let b = prompt('What rate you can do?', '');
-        if(a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50){
-            personalMovieDB.movies[a] = b;
-        }else{
-            console.log('You got error')
-            i--;
-        }
+function rememberMyFilms(){
+    for(let i = 0; i < 2; i++){
+        let a = prompt('what is the name of film?', '');
+        let b = prompt('What rate you can do?', '');
+            if(a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50){
+                personalMovieDB.movies[a] = b;
+            }else{
+                console.log('You got error');
+                i--;
+            }
+    }
 }
-if(personalMovieDB.count < 10){
-    alert('watched few films')
-}else if(personalMovieDB.count > 10 && personalMovieDB.count < 30){
-    alert('You are basic viewer')
-}else{
-    alert('You are movie fun')
+rememberMyFilms();
+
+
+function detectPersonalLevel(){
+    if(personalMovieDB.count < 10){
+        alert('watched few films');
+    }else if(personalMovieDB.count > 10 && personalMovieDB.count < 30){
+        alert('You are basic viewer');
+    }else{
+        alert('You are movie fun');
+    }
 }
-console.log(personalMovieDB);
+detectPersonalLevel();
+
+function showMyDB(){
+    if(personalMovieDB.privat == false){
+        console.log(personalMovieDB);
+    }else{}
+}
+showMyDB(); //My variant 
+
+// function showMyDB(hidden){
+//     if(!hidden){
+//         console.log(personalMovieDB);
+//     }else{}
+// }
+// showMyDB(personalMovieDB.privat);  //---Variant from video
+
+function writeYourGenres(){
+    for(let i = 1; i <= 3; i++){
+        let yourGenres = prompt(`Your favorite genre have a number ${i}`);
+        personalMovieDB.genres[i - 1] = yourGenres;
+    }
+}
+writeYourGenres();
